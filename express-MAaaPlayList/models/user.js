1a287c1728ca45f838c84a6f1c28e14cc11f6717
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-02-08 12:58:18 +0300

    create user schema

---
+++ express-MAaaPlayList/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      required: true
+    },
+    googleId: {
+      type: String,
+      required: true
+    },
+    playlists: [{type: Schema.Types.ObjectId, ref: 'Playlist' }]
+  },
+  {
+    timestamps: true
+  }
+)
+module.exports = mongoose.model('User', userSchema)
