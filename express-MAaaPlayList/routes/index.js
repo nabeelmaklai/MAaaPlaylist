8ec95e639822119e5e4c2a8ae3d64868eef78a4e
Mariam Mohammad Ali <maryamhoth@gmail.com>
2024-02-10 17:36:24 +0000

    add google oAuth feature and updated visuals

--- express-MAaaPlayList/routes/index.js
+++ express-MAaaPlayList/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express')
 var router = express.Router()
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'MAaaPlaylist' })
+})
+
+router.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+// Google OAuth callback route
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/error'
+  })
+)
+
+// OAuth logout route
+router.get('/logout', function (req, res) {
+  req.logout(function () {
+    res.redirect('/')
+  })
 })
 
 module.exports = router
