86b6b6113d4bf98819a1f07c0452c74042ba4a36
Mariam Mohammad Ali <maryamhoth@gmail.com>
2024-02-11 14:18:44 +0000

    update add songs to db

--- express-MAaaPlayList/models/song.js
+++ express-MAaaPlayList/models/song.js
@@ -1,21 +1,19 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
 const songSchema = new Schema(
   {
-    name: { type: String, required: true },
-    genre: [{ type: String, required: true }],
-    artist: [{ type: String, required: true }],
-    releaseYear: { type: Number, required: true },
-    cover: { type: String, required: true }
+    apiID: { type: String, required: true },
+    title: { type: String, required: true },
+    duration: { type: Number, required: true },
+    artist: { type: String, required: true },
+    releaseDate: { type: String, required: true },
+    cover: { type: String, required: true },
+    songPreview: { type: String, required: true },
+    explicitLyric: { type: Boolean, required: true }
   },
   {
     timestamps: true
   }
 )
 module.exports = mongoose.model('Song', songSchema)
-  
-
-  
-
