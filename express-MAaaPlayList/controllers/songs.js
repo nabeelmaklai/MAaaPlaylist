ccacbf83f6b70ecdfa59d6f72585e68d669a94ed
Mariam Mohammad Ali <maryamhoth@gmail.com>
2024-02-11 12:51:16 +0000

    updated search function to get results from api

--- express-MAaaPlayList/controllers/songs.js
+++ express-MAaaPlayList/controllers/songs.js
@@ -1,27 +1,27 @@
 const Song = require('../models/song')
+const API_URL = 'https://api.deezer.com/search?q='
+const API_TRACK_URL = 'https://api.deezer.com/track/'
+const axios = require('axios')
 
 const show = async (req, res) => {
   try {
-    const songs = await Song.find({
-      name: { $regex: req.body.name, $options: 'i' }
+    const response = await axios.get(API_URL + req.body.name)
+    res.render('songs/songresults', {
+      title: 'Song Results',
+      songs: response.data.data,
+      searchQuery: req.body.name
     })
-    if (songs.length === 0) {
-      res.send('No songs found')
-    } else {
-      res.render('songs/songresults', {
-        title: 'Results',
-        songs,
-        searchQuery: req.body.name
-      })
-    }
   } catch (error) {
     console.log(error)
   }
 }
 
 const details = async (req, res) => {
-  const songdetails = await Song.findById(req.params.id)
-  res.render('songs/songdetails', { title: 'Details', songdetails })
+  const songdetails = await axios.get(API_TRACK_URL + req.params.id)
+  res.render('songs/songdetails', {
+    title: 'Details',
+    songdetails: songdetails.data
+  })
 }
 
 module.exports = {
